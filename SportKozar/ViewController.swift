428bbbbc89287412740b6163e3c503965afcb095
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2022-08-27 21:36:48 +0200

    Initial Commit

---
+++ SportKozar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportKozar
+//
+//  Created by Raman Kozar on 27/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
